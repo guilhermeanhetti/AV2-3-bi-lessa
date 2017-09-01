054d1f8426fd1a50a0051816384847e1b1c631ed
Guilherme Santana Anhetti <guilherme.anheti@gmail.com>
2017-09-01 14:13:45 -0300

    Add files via upload

--- New Unity Project/Assets/Editor/TagSelectorDrawer.cs
+++ New Unity Project/Assets/Editor/TagSelectorDrawer.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using UnityEditorInternal;
+using System;
 
 [CustomPropertyDrawer(typeof(TagSelectorAttribute))]
 public class TagSelectorDrawer : PropertyDrawer {
 	public int index;
 	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label){
 
+		index = System.Array.IndexOf (InternalEditorUtility.tags, property.stringValue);
 		index = EditorGUI.Popup(position,label.text,index,InternalEditorUtility.tags);
+		if(index >= 0){
+		property.stringValue = InternalEditorUtility.tags[index];
+		}
 	}
 
+	}
 
-}
